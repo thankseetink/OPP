428f3e26bb54f0d27531e2c559b5e372e2011f51
thankseetink <126450805+thankseetink@users.noreply.github.com>
2023-02-27 14:43:22 +0700

    Add files via upload

---
+++ nam thang.cpp
@@ -0,0 +1,100 @@
+//#include <iostream>
+//#include <math.h>
+//#include <iomanip>
+//using namespace std;
+//int main (){
+//	int r;
+//	float CV;
+//	float DT;
+//	cout << "BK la:";
+//	cin >> r;
+//	cout << fixed << setprecision (2) << "CV" << " " << 2*3.14*r << endl;
+//	cout << fixed << setprecision (2) << "DT" << " " << 3.14*r*r << endl;
+//return 0;
+//}
+
+//#include <iostream>
+//#include <math.h>
+//#include <iomanip>
+//using namespace std;
+//int main () {
+//	int n;
+//	cout << "nhap n:";
+//	cin >> n;
+//	if ((n%2)==0){
+//		cout << "Chan";
+//	}
+//	else{
+//		cout << "Le";
+//	}
+//	return 0;
+//}
+
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main (){
+//	int n;
+//	cout << "nhap n:";
+//	cin >> n;
+//	if (((n%3)==0) && ((n%5)==0)) {
+//		cout << "1";
+//    }
+//	else {
+//		cout << "0";
+//	}
+//return 0;
+//}
+
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+////int main () {
+////	int a;
+//	cout << "Nhap nam:";
+//	cin >> a;
+//	if (((a%4)==0) && ((a%100)!=0)){ 
+//		cout << "YES";
+//	}
+////	else if ((a<0) || (a==0)){
+////		cout << "INVALID";
+//	}
+//	else {
+//		cout << "NO";
+//    }
+//return 0;	
+//}
+
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//int main () {
+//	int t;
+//	int n;
+//	cout << "thang:";
+//	cin >> t;
+//	cout << "nam:";
+//	cin >> n;
+//	if 	((t<0) && (n<0) || (t<1) && (t>12)) {
+//		cout << "INVALID";
+//	} 
+//	else if ((t==1) || (t==3) || (t==5) || (t==7) || (t==8) || (t==10) || (t==12)) {
+//		cout << "31";
+//	}
+//	else if ((t==4) || (t==6) || (t==9) || (t==11)) {
+//		cout << "30";
+//	}
+//	else if (((n%4)==0) && ((n%100)!=0)){
+//		cout << "29";
+//    }
+//	else {
+//		cout << "28";
+//	}
+//return 0;
+//	
+//	
+//}
+
+
+
+
